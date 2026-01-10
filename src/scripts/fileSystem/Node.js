4b77311ec508a0f6746316560fbd2227e93ac9c5
Renan-G-projec <renanfartes@gmail.com>
2026-01-10 14:47:32 -0300

    Creating the abstract class for a node of fileSystem

---
+++ src/scripts/fileSystem/Node.js
@@ -0,0 +1,29 @@
+// The Node class abstracts fileSystem related metadata. It is a generic for a file or a folder.
+/**@abstract*/
+class Node {
+    /** @param {string} name @param {Folder} parent */
+    constructor(name, parent=null) {
+        this.name = name;
+        this.parent = parent;
+    }
+
+    /** @returns {string} The absolute path, of the file/folder */
+    getAbsolutePath() {
+        // In case we're in the root
+        if (this.parent == null) return "/";
+
+        let current = this;
+        const path = [];
+
+        while (true) {
+            path.unshift(current.name);
+            if (current.parent == null) break;
+            current = current.parent;
+        }
+
+        return "/" + path.join("/");
+    }
+
+}
+
+export default Node;
