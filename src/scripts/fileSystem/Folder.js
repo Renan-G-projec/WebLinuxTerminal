e11e5386fab8a0faca959a16bbc3f28fcdc61275
Renan-G-projec <renanfartes@gmail.com>
2026-01-10 15:19:39 -0300

    Adding a new mwthod for Folder and fixing some bugs

--- src/scripts/fileSystem/Folder.js
+++ src/scripts/fileSystem/Folder.js
@@ -11,9 +11,14 @@ class Folder extends Node {
         this.children.set(child.name, child); // Creating 2 nodes with the same name should be trated out the engine.
     }
 
+    /** @param {string} child */
+    getChild(child) {
+        return this.children.get(child); // Again, error treating should happen on the TerminalEngine.
+    }
+
     /** @returns {string[]} */
     listAllChildren() {
-        return Array.from(this.children).keys();
+        return Array.from(this.children.keys());
     }
 }
 
